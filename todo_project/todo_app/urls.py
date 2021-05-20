e948a842a56e29d869f90fb646520f55eeaee7d0
felna14 <francyfelna14@gmail.com>
2021-05-20 15:14:35 +0530

    todo app added

---
+++ todo_project/todo_app/urls.py
@@ -0,0 +1,29 @@
+"""todo_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from todo_app import views
+
+urlpatterns = [
+    path('',views.task_view,name='task_view'),
+    path('delete/<int:task_id>',views.delete,name='delete'),
+    path('update<int:id>/',views.update,name='update'),
+    path('cbvtask/',views.TaskListView.as_view(),name='cbvtask'),
+    path('cbvdetail/<int:pk>/', views.TaskDetailView.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.TaskDeleteView.as_view(), name='cbvdelete')
+]
